4f43e12831eee37cddf34308bea2dedb8ccc5101
PoojaBorad <poojabborad@gmail.com>
2023-09-14 13:21:54 -0700

    Update guess_num_with_limit.py

--- guess_num_with_limit.py
+++ guess_num_with_limit.py
@@ -3,4 +3,7 @@ print("Modify the 'Guess the Number' game to allow the user to set a maximum num
 import random
 
 def guess_number():
+    print("Welcome to the Game of the 'GUESS THE NUMBER'!")
+    print("I will choose a random number, and you have to guess it.")
+
     
