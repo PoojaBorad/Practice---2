51793a52ab78e7c70c57b74d06a2f575b4fce8fc
PoojaBorad <poojabborad@gmail.com>
2023-09-15 10:07:00 -0700

    Update guess_num_with_limit.py

--- guess_num_with_limit.py
+++ guess_num_with_limit.py
@@ -6,4 +6,10 @@ def guess_number():
     print("Welcome to the Game of the 'GUESS THE NUMBER'!")
     print("I will choose a random number, and you have to guess it.")
 
+    max_number = int(input("Enter the maximum you want to guess: "))
+    max_attempts = int(input("Enter the number of attempts you want in this game between 1 to 15: "))
+
+    secret_number = random.randint(1, max_number)
+    attempts = 0
+
     
